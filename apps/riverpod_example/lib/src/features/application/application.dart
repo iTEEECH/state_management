61763c28671911a952c7cab8ca824206efe4056e
Florentin <florentin.bouchez@gmail.com>
2025-02-20 15:47:57 +0100

    feat: Implement Riverpod

---
+++ apps/riverpod_example/lib/src/features/application/application.dart
@@ -0,0 +1,1 @@
+export 'presentation/application.dart';
---
+++ apps/riverpod_example/lib/src/features/home/application/home_controller.dart
@@ -0,0 +1,27 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_example/src/features/home/home.dart';
+
+part 'home_controller.g.dart';
+
+@riverpod
+class HomeController extends _$HomeController {
+  static const _kDefaultCountryName = 'France';
+
+  @override
+  Future<Country?> build() async {
+    return await fetchCountry(name: _kDefaultCountryName);
+  }
+
+  Future<Country?> fetchCountry({required String name}) async {
+    state = const AsyncLoading();
+
+    Future.delayed(const Duration(seconds: 2));
+
+    state = await AsyncValue.guard<Country>(() async {
+      final Country country = await ref.read(homeRepositoryProvider).fetchCountry(name: name);
+      return country;
+    });
+
+    return state.value;
+  }
+}
