c886df5baf61e42437773a1e6a70837fbd2c1a54
Florentin <florentin.bouchez@gmail.com>
2025-02-20 22:05:42 +0100

    feat: Implement Riverpod

---
+++ apps/riverpod_example/lib/src/features/home/home.dart
@@ -0,0 +1,11 @@
+export 'application/home_controller.dart';
+export 'data/model/country_model.dart';
+export 'data/source/home_remote.dart';
+export 'data/source/home_remote_impl.dart';
+export 'data/repository/home_repository_impl.dart';
+export 'domain/entity/country_entity.dart';
+export 'domain/repository/home_repository.dart';
+export 'presentation/widget/home_button.dart';
+export 'presentation/widget/home_image.dart';
+export 'presentation/widget/home_text_field.dart';
+export 'presentation/home_page.dart';
