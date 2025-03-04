f6f4546945330dbead8c916feba1ecf75286374a
Florentin <florentin.bouchez@gmail.com>
2025-03-04 15:24:42 +0100

    feat: Implement Cubit

---
+++ apps/cubit_example/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:cubit_example/inject.dart';
+import 'package:cubit_example/src/features/application/application.dart';
+
+void main() {
+  setup();
+  runApp(const Application());
+}
