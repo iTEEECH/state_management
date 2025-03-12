4fb009ef11f38e19e335e80a218f84a26d4cbf25
Florentin <florentin.bouchez@gmail.com>
2025-03-12 17:33:21 +0100

    feat: Implement Riverpod

---
+++ apps/riverpod_example/lib/src/features/home/data/repository/home_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_example/src/features/home/home.dart';
+
+final class HomeRepositoryImpl implements HomeRepository {
+  /// Used by providers to interact with other providers
+  /// and the life-cycles of the application.
+  final Ref _ref;
+
+  const HomeRepositoryImpl({required Ref ref}) : _ref = ref;
+
+  @override
+  Future<Country> fetchCountry({required String name}) async{
+    final CountryModel result = await _ref.read(homeRemoteProvider).fetchCountry(name);
+    return result.toEntity();
+  }
+}
