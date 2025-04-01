80c647292b89949b1f4d4a057d6f9cd2f22a0679
Florentin <florentin.bouchez@gmail.com>
2025-04-01 19:12:15 -0400

    feat: Implement Flutter BLoC

---
+++ apps/flutter_bloc_example/lib/src/features/home/application/home_bloc.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:flutter_bloc_example/src/features/home/home.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  final HomeRepository _homeRepository;
+
+  static const _kDefaultCountry = 'France';
+
+  HomeBloc(this._homeRepository) : super(HomeInitial()) {
+    on<FetchCountryEvent>(_fetchCountry);
+  }
+
+  Future<void> _fetchCountry(FetchCountryEvent event, Emitter<HomeState> emit) async {
+    emit(HomeLoading());
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    final Country country = await _homeRepository.fetchCountry(name: event.name ?? _kDefaultCountry);
+
+    if (!isClosed) {
+      emit(HomeSuccess(country));
+    }
+  }
+}ede
