85dcce6138bbacf7b9d519e81f3fab8de1884e8c
Florentin Bouchez <florentin.bouchez@gmail.com>
2025-04-16 16:28:11 -0400

    feat: Implement GetX

---
+++ apps/get_example/lib/src/features/home/application/home_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:get_example/src/features/home/home.dart';
+
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeRemote>(() => HomeRemoteImpl());
+    Get.lazyPut<HomeRepository>(() => HomeRepositoryImpl(remote: Get.find()));
+    Get.lazyPut(() => HomeController(repository: Get.find()));
+  }
+}
