5fed8e6ec9453c9f4132bbc1ce9c2c10ad9cac00
Florentin Bouchez <florentin.bouchez@gmail.com>
2025-04-16 16:28:39 -0400

    feat: Implement Riverpod

---
+++ apps/riverpod_example/lib/src/features/home/data/source/home_remote.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_remote.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$homeRemoteHash() => r'acc1b3f9ee27948ac57076d4f1e2384bc74a5f5c';
+
+/// See also [homeRemote].
+@ProviderFor(homeRemote)
+final homeRemoteProvider = AutoDisposeProvider<HomeRemote>.internal(
+  homeRemote,
+  name: r'homeRemoteProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$homeRemoteHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef HomeRemoteRef = AutoDisposeProviderRef<HomeRemote>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
