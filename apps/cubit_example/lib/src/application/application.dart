f453f4880179eaddd4687ef0d03423e0429644bb
Florentin Bouchez <florentin.bouchez@gmail.com>
2025-04-16 16:27:15 -0400

    feat: Implement Cubit

---
+++ apps/cubit_example/lib/src/application/application.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:cubit_example/inject.dart';
+import 'package:cubit_example/src/features/home/home.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cubit Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: BlocProvider(
+        create: (context) => getIt<HomeBloc>()..fetchCountry(),
+        child: Builder(builder: (context) => const HomePage()),
+      ),
+    );
+  }
+}
