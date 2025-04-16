3eed06f63bf52e4b36253f86d4648c50c16d5e4f
Florentin Bouchez <florentin.bouchez@gmail.com>
2025-04-16 16:27:42 -0400

    feat: Implement Flutter BLoC

---
+++ apps/flutter_bloc_example/README.md
@@ -0,0 +1,16 @@
+# Flutter BLoC
+
+## Summary
+- 🚀 Platforms
+- 📃 Description
+- ⚙️ Setup
+- 💻 Use case
+
+## Platforms
+| Android | iOS | Web |
+|:-------:|:---:|:---:|
+|    ✅    |  ✅  |  ✅   |
+
+## Description
+
+This Flutter project demonstrates a simple project with Flutter BLoC.
---
+++ apps/flutter_bloc_example/lib/src/features/home/presentation/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_example/src/features/home/home.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter BLoC Example'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            spacing: 40.0,
+            children: <Widget>[
+              HomeImage(),
+              HomeTextField(controller: _controller),
+              HomeButton(controller: _controller),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
